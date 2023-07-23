c5322d40f3abf0a43867662ba43a3e6e9807b597
chethanavirajini <cheth.virajini@gmail.com>
2023-07-23 22:20:07 +0530

    Guess game 2

---
+++ Python-prac-demo/test4.py
@@ -0,0 +1,33 @@
+# using for loop
+
+for i in range(0, 9):
+    print(i)
+
+print(".......................................")
+# using while loop
+
+count = 0
+while count < 9:
+    print(count)
+    count = count + 1
+print("Done")
+
+print(".......................................")
+# guessing game
+
+secret_number = 13
+chance = 0
+
+
+while chance < 3:
+    guess_number = int(input("input number: "))
+    if guess_number < 13:
+        print("Number is too small")
+    elif guess_number > 13:
+        print("Number is too large")
+    elif guess_number == 13:
+        print("Congratulations, You won")
+        break
+    chance = chance + 1
+else:
+    print("Better luck next time")
