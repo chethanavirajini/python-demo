54d1fbb8b4a4991d968694332a934b432ef63854
chethanavirajini <cheth.virajini@gmail.com>
2023-07-23 11:34:08 +0530

    Arithmetic operations

--- test5.py
+++ test5.py
@@ -1,1 +1,28 @@
 # Arithmetic operations
+# addition, subtraction, multiplication, division, remainder, Exponentiation, Floor division
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+# augmented operations
+x = 10
+# x = x + 3
+
+x += 3     # augmented or enhance the operational code with less memory
+x -= 3
+x *= 3
+
+print(x)
+
+
+# operator precedence
+
+y = 10 + 3 * 2
+z = (10 + 3) * 2
+print(y)
+print(z)
