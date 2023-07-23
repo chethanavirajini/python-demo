f171091a0d91f3f133408e590339f2cf15bb262c
chethanavirajini <cheth.virajini@gmail.com>
2023-07-23 12:54:17 +0530

    while loop

---
+++ test8.py
@@ -0,0 +1,24 @@
+# loops
+# while loop to repeat block of codes multiple times
+
+# print 1 to 5
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")   # but this is a bad approach
+
+print("...............................")
+
+i = 1
+while i <= 500:
+    print(i)
+    i += 1
+
+print(15 * ".")
+
+i = 1
+while i <= 10:
+    print(i * "*")   # string and number can multiply. String will be multiply based on the number
+    i = i + 1
+
