ad382d121d8d4d616adfcd14bdb65607b460c2c6
chethanavirajini <cheth.virajini@gmail.com>
2023-07-23 11:50:48 +0530

    Comparison and logical operations

---
+++ test6.py
@@ -0,0 +1,28 @@
+# comparison operators > < >= <= == !=
+
+x = 5
+y = 8
+
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+print(x == y)
+print(x != y)
+
+print("..................................")
+# logical operators  AND, OR , NOT
+
+price = 5
+
+print(price > 10 and price < 30)
+print(price > 10 or price < 30)
+print(not(price > 10))
+
+
+
+
+
+
+
+
