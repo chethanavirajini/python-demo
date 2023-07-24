530a23c906e1be591ebdc30f52197781c429930f
chethanavirajini <cheth.virajini@gmail.com>
2023-07-24 10:54:08 +0530

    Basic problem 1

--- Python exercises/geeksproblems/Basic1.py
+++ Python exercises/geeksproblems/Basic1.py
@@ -1,6 +1,34 @@
+# method 1
 def sum(x, y):
     total = x + y
     return total
 
 print(sum(10, 15))
+
+print("........................................")
+
+# method 2
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+def total(a, b):
+    sum = a + b
+    return sum
+print(total(a,b))
+
+# method 3
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+Total = float(x) + float(y)
+
+print(f'Sum of {x} and {y} is {Total}')
+
+
+
+
+
+
 
